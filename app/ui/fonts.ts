39145f570d405f0a506794aa2a883951024d2858
Yeison Silva <yeisonsilvat06@gmail.com>
2025-04-25 12:18:52 -0500

    next.js course

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from 'next/font/google'; //fuentes de google
+
+
+export const inter = Inter({
+  subsets: ['latin'] }); //subconjunto de caracteres latinos
+
+export const lusitana = Lusitana({ //fuente lucitana
+    weight: ['400', '700'],  //peso de la fuente
+    subsets: ['latin'], //subconjunto de caracteres latinos
+});
